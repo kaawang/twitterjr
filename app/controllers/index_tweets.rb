3dc599e0e443e18c130dac88e6dbf29580264f23
William Tsang <williamkt91@gmail.com>
2015-01-15 18:26:54 -0800

    Commented out this file since we merged it to main index.rb

--- app/controllers/index_tweets.rb
+++ app/controllers/index_tweets.rb
@@ -1,16 +1,19 @@
-get '/tweets/new' do
-  erb :'tweets/new_tweet_form'
-end
+#problem found at line 14 and merged to index.rb
+#fixed problem in index.rb
 
-post '/tweets/new' do
-  tweet = Tweet.create(params[:tweet])
-  redirect '/tweets/show'
-end
+# get '/tweets/new' do
+#   erb :'tweets/new_tweet_form'
+# end
 
-get '/tweets/show' do
-  @all_tweets = Tweet.pluck(:content).reverse
-  erb :'tweets/show'
-end
+# post '/tweets/new' do
+#   tweet = Tweet.create(params[:tweet])
+#   redirect '/tweets/show'
+# end
+
+# get 'users/:id/tweets/show' do
+#   @all_tweets = Tweet.pluck(:content).reverse
+#   erb :'tweets/show'
+# end
 
 
 
